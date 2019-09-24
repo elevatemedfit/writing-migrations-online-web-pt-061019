da790dcc3e84d9412b0b556a3c0311fb0a112189
Chris Junker <elevatemedfit@gmail.com>
2019-09-24 16:26:41 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
     add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string
+    add_column :students, :birthdate, :string#add acolumn to students with birthdate as string.
   end
 end
--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change
-    change_column :students, :birthdate, :datetime
+    change_column :students, :birthdate, :datetime #students is the table, birthdate as datetime vs. integer
   end
 end
